610eefcb2d8c2cb1aa273fe4d26a92960791c5c6
mohashraf148932e7 <ashraf.m.1414@gmail.com>
2025-03-20 00:03:27 +0200

    initialize our 2 pointers to point on head

---
+++ LL/876. Middle of the Linked List.py
@@ -0,0 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        slow, fast = head, head
