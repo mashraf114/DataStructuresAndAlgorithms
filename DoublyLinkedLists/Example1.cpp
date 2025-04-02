5885183dae1a57509116edd19322a78e81f591e6
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2025-04-02 11:45:52 +0200

    Member functions get Head, tail and length

--- DoublyLinkedLists/Example1.cpp
+++ DoublyLinkedLists/Example1.cpp
@@ -26,11 +26,34 @@ class DoublyLinkedList{
       tail = newNode;
       length = 1;
     }
+    void printList(){
+      Node* temp = head;
+      while(temp){
+        cout << temp->value << endl;
+        temp = temp->next;
+
+        };
+     };
+    
+     void getHead(){
+      cout << "Head: " << head->value << endl;
+     }
+      void getTail(){
+        cout << "Tail: " << tail->value << endl;
+      }
+      void getLength(){
+        cout << "Length: " << length << endl;
+      }
 };
 
 
 int main(){
   DoublyLinkedList* myDLL = new DoublyLinkedList(10);
-
+  // myLinkedList->append(20);
+  // myLinkedList->append(30);
+  myDLL->printList();
+  myDLL->getHead();
+  myDLL->getTail();
+  myDLL->getLength();
   return 0;
 }
